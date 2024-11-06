40d7739db281923a7a7a2342229344f6e6201918
nagasakimark <m.pook022@g.nagasaki-city.ed.jp>
2024-11-06 20:42:23 +0900

    Add files via upload

---
+++ src/constants.js
@@ -0,0 +1,326 @@
+export const bgImages = [
+  "https://images.unsplash.com/photo-1598153346810-860daa814c4b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8aGFycnklMjBwb3R0ZXJ8ZW58MHx8MHx8&auto=format&fit=crop",
+  "https://images.unsplash.com/photo-1547756536-cde3673fa2e5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8aGFycnklMjBwb3R0ZXJ8ZW58MHx8MHx8&auto=format&fit=crop",
+  "https://images.unsplash.com/photo-1583997052301-0042b33fc598?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8aGFycnklMjBwb3R0ZXJ8ZW58MHx8MHx8&auto=format&fit=crop",
+  "https://images.unsplash.com/photo-1548630826-2ec01a41f48f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8aGFycnklMjBwb3R0ZXJ8ZW58MHx8MHx8&auto=format&fit=crop",
+  "https://images.unsplash.com/photo-1618944847828-82e943c3bdb7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8aGFycnklMjBwb3R0ZXJ8ZW58MHx8MHx8&auto=format&fit=crop",
+  "https://images.unsplash.com/photo-1618944913480-b67ee16d7b77?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8N3x8aGFycnklMjBwb3R0ZXJ8ZW58MHx8MHx8&auto=format&fit=crop",
+  "https://images.unsplash.com/photo-1571936804022-90d128047136?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTV8fGhhcnJ5JTIwcG90dGVyfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop",
+  "https://images.unsplash.com/photo-1533462506003-13c20d204b58?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTd8fGhvZ3dhcnRzfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop",
+  "https://images.unsplash.com/photo-1626914535205-8e4e3bf36793?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NjN8fGhvZ3dhcnRzfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop",
+  "https://images.unsplash.com/photo-1619622338668-484d0f2e1c3b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Njd8fGhvZ3dhcnRzfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop",
+  "https://images.unsplash.com/photo-1539103377911-4909a1eae382?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTR8fGhhcnJ5JTIwcG90dGVyfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop",
+  "https://images.unsplash.com/photo-1610466024868-910c6e7e8929?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTZ8fGhhcnJ5JTIwcG90dGVyfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop",
+  "https://images.unsplash.com/photo-1632266484284-a11d9e3a460a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTd8fGhhcnJ5JTIwcG90dGVyfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop",
+  "https://images.unsplash.com/photo-1575887339850-1acc9d8daf3e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTl8fGhhcnJ5JTIwcG90dGVyfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop",
+  "https://images.unsplash.com/photo-1618945034861-7e61e0326e74?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Njl8fGhhcnJ5JTIwcG90dGVyfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop",
+  "https://images.unsplash.com/photo-1498676077434-7540603d2dda?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8ODh8fGhhcnJ5JTIwcG90dGVyfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop",
+];
+export const housesInfo = [
+  { houseName: "Slytherin", houseCrest: "./assets/crest-slytherin.png" },
+  { houseName: "Gryffindor", houseCrest: "./assets/crest-gryffindor.png" },
+  { houseName: "Ravenclaw", houseCrest: "./assets/crest-ravenclaw.png" },
+  { houseName: "Hufflepuff", houseCrest: "./assets/crest-hufflepuff.png" },
+];
+export const allQuestions = [
+  {
+    question: "Which word below describes you best?",
+    answers: {
+      slytherin: "Cunning / 狡猾",
+      gryffindor: "Brave / 勇敢",
+      ravenclaw: "Clever / 賢明",
+      hufflepuff: "Friendly / 親切",
+    },
+    weight: 4,
+  },
+  {
+    question: "Of these, which animal do you like best?",
+    answers: {
+      slytherin: "Snake / 蛇",
+      gryffindor: "Lion / ライオン",
+      ravenclaw: "Raven / カラス",
+      hufflepuff: "Badger / アナグマ",
+    },
+    weight: 2,
+  },
+  {
+    question: "What's your favorite colour?",
+    answers: {
+      slytherin: "Green / 緑",
+      gryffindor: "Scarlet / 深紅",
+      ravenclaw: "Blue / 青",
+      hufflepuff: "Yellow / 黄",
+    },
+    weight: 1,
+  },
+  {
+    question: "What do you find the most interesting?",
+    answers: {
+      slytherin: "Hexes / 呪い",
+      gryffindor: "Broom Flying / ほうき飛行",
+      ravenclaw: "Transfiguration / 変身術",
+      hufflepuff: "Care of Magical Creatures / 魔法生物飼育学",
+    },
+    weight: 2,
+  },
+  {
+    question: "Which famous Hogwarts person would you like to meet?",
+    answers: {
+      slytherin: "Tom Riddle / トム・リドル",
+      gryffindor: "Albus Dumbledore / アルバス・ダンブルドア",
+      ravenclaw: "Garrick Olivander / ガリック・オリバンダー",
+      hufflepuff: "Cedric Diggory / セドリック・ディゴリー",
+    },
+    weight: 1,
+  },
+  {
+    question: "What's your favorite movie genre?",
+    answers: {
+      slytherin: "Crime / 犯罪",
+      gryffindor: "Action / アクション",
+      ravenclaw: "Documentary / ドキュメンタリー",
+      hufflepuff: "Comedy / コメディ",
+    },
+    weight: 2,
+  },
+  {
+    question: "How do you see yourself in the Mirror of Erised (みぞの鏡)?",
+    answers: {
+      slytherin: "with riches / 富と共に",
+      gryffindor: "on an adventure / 冒険の最中",
+      ravenclaw: "with a book / 本と共に",
+      hufflepuff: "with friends & family / 家族や友達と共に",
+    },
+    weight: 3,
+  },
+  {
+    question: "Of these four, which muggle (マグル) job would you choose?",
+    answers: {
+      slytherin: "Salesperson / 営業職",
+      gryffindor: "Firefighter / 消防士",
+      ravenclaw: "Researcher / 研究者",
+      hufflepuff: "Caregiver / 介護士",
+    },
+    weight: 3,
+  },
+  {
+    question: "What is your wand made from?",
+    answers: {
+      slytherin: "Dragon heartstring / ドラゴンの心臓の筋",
+      gryffindor: "Phoenix feather / フェニックスの羽根",
+      ravenclaw: "Veela hair / ヴィーラの髪",
+      hufflepuff: "Unicorn hair / ユニコーンの毛",
+    },
+    weight: 2,
+  },
+  {
+    question: "What part of Hogwarts do you want to explore?",
+    answers: {
+      slytherin: "The Forbidden Forest / 禁じられた森",
+      gryffindor: "The Room of Requirement / 必要の部屋",
+      ravenclaw: "The Library / 図書館",
+      hufflepuff: "The Kitchen / 厨房",
+    },
+    weight: 1,
+  },
+  {
+    question: "What do you like most about other people?",
+    answers: {
+      slytherin: "Ambition / 野心",
+      gryffindor: "Sense of adventure / 冒険心",
+      ravenclaw: "Intellect / 知性",
+      hufflepuff: "Kindness / 優しさ",
+    },
+    weight: 4,
+  },
+  {
+    question: "Which is your favorite Dumbledore quote?",
+    answers: {
+      slytherin: '"It matters not what someone is born, but what they grow to be." / "人は生まれではなく、成長によって決まるのだ"',
+      gryffindor: '"It does not do to dwell on dreams and forget to live." / "夢にふける一方で生きることを忘れてはいけない"',
+      ravenclaw: '"Words are, in my not-so-humble opinion, our most inexhaustible source of magic." / "私の謙虚とは言えない意見だが、言葉は我々の最も尽きることのない魔法の源なのだ"',
+      hufflepuff: '"Pity the living. And above all those who live without love." / "生きている者を哀れむがいい。そして何より、愛なしに生きる者たちを"',
+    },
+    weight: 3,
+  },
+  {
+    question: "What element (元素) do you like?",
+    answers: {
+      slytherin: "Fire / 火",
+      gryffindor: "Earth / 地",
+      ravenclaw: "Air / 風",
+      hufflepuff: "Water / 水",
+    },
+    weight: 1,
+  },
+  {
+    question: "How would others describe you?",
+    answers: {
+      slytherin: "Ambitious / 野心的",
+      gryffindor: "Honorable / 誠実",
+      ravenclaw: "Curious / 好奇心旺盛",
+      hufflepuff: "Humble / 謙虚",
+    },
+    weight: 4,
+  },
+  {
+    question: "What would you rather be?",
+    answers: {
+      slytherin: "Praised / 称賛される",
+      gryffindor: "Respected / 尊敬される",
+      ravenclaw: "Admired / 感嘆される",
+      hufflepuff: "Trusted / 信頼される",
+    },
+    weight: 4,
+  },
+  {
+    question: "You meet a troll on a bridge you want to cross; how do you deal with it?",
+    answers: {
+      slytherin: "kill it / 殺す",
+      gryffindor: "fight it fairly / 正々堂々と戦う",
+      ravenclaw: "trick it / だます",
+      hufflepuff: "negotiate with it to let you pass / 通してもらえるよう交渉する",
+    },
+    weight: 3,
+  },
+  {
+    question: "Which relaxing activity do you like most?",
+    answers: {
+      slytherin: "improving myself / 自己啓発",
+      gryffindor: "trying something new / 新しいことに挑戦",
+      ravenclaw: "reading / 読書",
+      hufflepuff: "volunteering / ボランティア活動",
+    },
+    weight: 2,
+  },
+  {
+    question: "Which of these animals do you want as a pet?",
+    answers: {
+      slytherin: "lizard / トカゲ",
+      gryffindor: "dog / 犬",
+      ravenclaw: "cat / 猫",
+      hufflepuff: "bunny / ウサギ",
+    },
+    weight: 1,
+  },
+  {
+    question: "If you could make a potion that would give you one thing, what would it be?",
+    answers: {
+      slytherin: "Fame / 名声",
+      gryffindor: "Respect / 尊敬",
+      ravenclaw: "Wisdom / 知恵",
+      hufflepuff: "Love / 愛",
+    },
+    weight: 1,
+  },
+  {
+    question: "You find a bag with 10,000 galleons (ガリオン) in it; what do you do with it?",
+    answers: {
+      slytherin: "Keep it & tell no one / 黙って持ち帰る",
+      gryffindor: "Go to the police with it / 警察に届ける",
+      ravenclaw: "Keep it & make a strategic investment / 持ち帰って戦略的に投資する",
+      hufflepuff: "Try to find the owner / 持ち主を探す",
+    },
+    weight: 3,
+  },
+  {
+    question: "You would be most sad if someone called you...",
+    answers: {
+      slytherin: "Lazy / 怠け者",
+      gryffindor: "Cowardly / 臆病者",
+      ravenclaw: "Ignorant / 無知",
+      hufflepuff: "Inconsiderate / 思いやりのない",
+    },
+    weight: 5,
+  },
+  {
+    question: "What is the most likely reason you'd receive a Howler (吼えメール)?",
+    answers: {
+      slytherin: "Cheating on an exam / 試験でカンニング",
+      gryffindor: "Sneaking into the Forbidden Forest / 禁じられた森に忍び込む",
+      ravenclaw: "Staying in the library past closing time / 図書館の閉館時間を過ぎても残る",
+      hufflepuff: "I would never, ever receive one / 絶対に受け取ることはない",
+    },
+    weight: 5,
+  },
+  {
+    question: "Which of these jobs would you like to have?",
+    answers: {
+      slytherin: "Minister of Magic / 魔法大臣",
+      gryffindor: "Auror / 闇祓い",
+      ravenclaw: "Professor / 教授",
+      hufflepuff: "Dragon Keeper / ドラゴン使い",
+    },
+    weight: 4,
+  },
+  {
+    question: "You notice a friend cheating on an exam; what do you do?",
+    answers: {
+      slytherin: "Offer to let them cheat off of your test next time / 次回は自分の答案を写させてあげる",
+      gryffindor: "Tell the professor / 教授に告げる",
+      ravenclaw: "Offer to help them study for the next one / 次回の試験の勉強を手伝う",
+      hufflepuff: "Nothing / 何もしない",
+    },
+    weight: 3,
+  },
+  {
+    question: "Would you ever use a forbidden spell (禁忌呪文)?",
+    answers: {
+      slytherin: "If it served me, & I could get away with it, yes / 自分の役に立ち、バレなければ使う",
+      gryffindor: "To save my life or the lives of others, yes / 自分や他人の命を救うためなら使う",
+      ravenclaw: "I shouldn't ever need to use one / 使う必要はないはず",
+      hufflepuff: "No, that's illegal! / いいえ、違法です！",
+    },
+    weight: 6,
+  },
+  {
+    question: "After you have died, how do you want people to remember you?",
+    answers: {
+      slytherin: "Think with admiration of your achievements / 私の功績を賞賛して思い出す",
+      gryffindor: "Ask for more stories about your adventures / 私の冒険についてもっと話を聞きたがる",
+      ravenclaw: "I don't care what people think of me after I'm dead, it's what they think of me while I'm alive that counts / 死後の評価は気にしない。生きている間の評価が大切",
+      hufflepuff: "Miss you, but smile / 寂しく思うけれど、笑顔で",
+    },
+    weight: 4,
+  },
+  {
+    question: "If you could have one of these powers, which would you choose?",
+    answers: {
+      slytherin: "Read others' minds / 他人の心を読む",
+      gryffindor: "Invisibility / 透明になる",
+      ravenclaw: "Time travel / 時間旅行",
+      hufflepuff: "Ability to talk with animals / 動物と話せる",
+    },
+    weight: 1,
+  },
+  {
+    question: "Which nightmare (悪夢) would frighten you most?",
+    answers: {
+      slytherin: "Being publicly embarrassed / 人前で恥をかく",
+      gryffindor: "Being trapped on a high-up place with nothing to stop you from falling / 高所で転落を防ぐものが何もない",
+      ravenclaw: "An eye at the keyhole of the dark, windowless room in which you are locked / 暗い窓のない部屋に閉じ込められ、鍵穴に目が光っている",
+      hufflepuff: "Your loved ones forget who you are / 大切な人があなたのことを忘れてしまう",
+    },
+    weight: 2,
+  },
+  {
+    question: "Choose one of these things to do for the rest of your life",
+    answers: {
+      slytherin: "Start your own company / 起業する",
+      gryffindor: "Explore the world / 世界を探検する",
+      ravenclaw: "Stay in school / 学び続ける",
+      hufflepuff: "Raise a family / 家族を育てる",
+    },
+    weight: 5,
+  },
+  {
+    question: "Which house do you hope to be in?",
+    answers: {
+      slytherin: "Slytherin / スリザリン",
+      gryffindor: "Gryffindor / グリフィンドール",
+      ravenclaw: "Ravenclaw / レイブンクロー",
+      hufflepuff: "Hufflepuff / ハッフルパフ",
+    },
+    weight: 8,
+  },
+];
---
+++ src/main.js
@@ -0,0 +1,148 @@
+import { bgImages as e, housesInfo as n, allQuestions as t } from "./constants.js";
+var btn,
+  _a,
+  setRandBG = function () {
+    var n = Math.floor(Math.random() * e.length);
+    document.body.style.backgroundImage = "url(" + e[n] + ")";
+  };
+setRandBG();
+for (
+  var shuffleArray = function (e) {
+      for (var n, t = 1; t < e.length; t++) {
+        var r = Math.floor(Math.random() * (t + 1));
+        (n = [e[r], e[t]]), (e[t] = n[0]), (e[r] = n[1]);
+      }
+      return e;
+    },
+    currentIndex = -1,
+    app = document.querySelector("#app"),
+    greeting = document.querySelector("#greeting"),
+    questionArea = document.querySelector("#question-area"),
+    resultsArea = document.querySelector("#results-area"),
+    housesAndTheirTotals =
+      (((_a = {})["".concat(n[0].houseName.toLowerCase())] = 0),
+      (_a["".concat(n[1].houseName.toLowerCase())] = 0),
+      (_a["".concat(n[2].houseName.toLowerCase())] = 0),
+      (_a["".concat(n[3].houseName.toLowerCase())] = 0),
+      _a),
+    allocatePoints = function (e, n) {
+      housesAndTheirTotals["".concat(e)] += n;
+    },
+    quizLength = 30,
+    handleLengthSelection = function (e) {
+      "short" === e && (quizLength = 15);
+    },
+    proceed = function () {
+      if ((currentIndex++, setRandBG(), questionsArray[currentIndex])) {
+        0 === currentIndex &&
+          (app.removeChild(greeting), (questionArea.style.display = "flex"));
+        for (
+          var e = "",
+            t = shuffleArray(Object.entries(questionsArray[currentIndex].answers)),
+            r = 0,
+            o = t;
+          r < o.length;
+          r++
+        ) {
+          var a = o[r];
+          e += '<button class="option-btn" id="'
+            .concat(a[0], '">')
+            .concat(a[1], "</button>");
+        }
+        currentIndex > 0 &&
+          currentIndex !== questionsArray.length &&
+          (questionArea.innerHTML = ""),
+          (questionArea.innerHTML += '\n        <div class="question">\n        <header>'
+            .concat(questionsArray.indexOf(questionsArray[currentIndex]) + 1, " / ")
+            .concat(
+              questionsArray.length,
+              "\n        </header>\n        <header>\n        "
+            )
+            .concat(
+              questionsArray[currentIndex].question,
+              '\n        </header>\n        <div id="options-area">\n        '
+            )
+            .concat(e, "\n        </div>\n        </div>\n      "));
+        for (
+          var s = document.querySelectorAll(".option-btn"),
+            i = function (e) {
+              e.addEventListener("click", function () {
+                allocatePoints(e.id, questionsArray[currentIndex].weight), proceed();
+              });
+            },
+            c = 0,
+            u = s;
+          c < u.length;
+          c++
+        )
+          i(u[c]);
+      }
+      if (currentIndex === questionsArray.length) {
+        var d = function () {
+          var e = Object.values(housesAndTheirTotals).map(function (e) {
+              return e;
+            }),
+            t = Math.max.apply(Math, e);
+          return n[e.indexOf(t)];
+        };
+        app.removeChild(questionArea),
+          (document.body.style.backgroundImage =
+            "url(/assets/sortinghat.jpg)"),
+          window.innerWidth <= 1024 &&
+            window.innerWidth > 900 &&
+            (document.body.style.backgroundPositionX = "-33rem"),
+          window.innerWidth <= 900 &&
+            window.innerWidth > 615 &&
+            (document.body.style.backgroundPositionX = "-54rem"),
+          window.innerWidth <= 615 &&
+            (document.body.style.backgroundPositionX = "-27rem"),
+          (resultsArea.style.display = "flex"),
+          (resultsArea.innerHTML +=
+            '\n      <header>After much deliberation, the sorting hat has decided to place you in...</header>\n      <div id="results-info-container">\n        <img src="'
+              .concat(d().houseCrest, '">\n        <p>')
+              .concat(
+                d().houseName,
+                '</p>\n        </div>\n        <button id="retake-quiz-btn" onclick="window.location.reload()">Retake Quiz!</button>\n      '
+              ));
+        var l = document.querySelector("#results-area header");
+        l.classList.add("animate__animated"), l.classList.add("animate__fadeInLeft");
+        var h = document.getElementById("results-info-container");
+        h.classList.add("animate__animated"), h.classList.add("animate__jackInTheBox");
+        var f = document.getElementById("retake-quiz-btn");
+        setTimeout(function () {
+          (f.style.display = "block"),
+            f.classList.add("animate__animated"),
+            f.classList.add("animate__jackInTheBox");
+        }, 3e3),
+          setTimeout(function () {
+            f.classList.remove("animate__animated"),
+              f.classList.remove("animate__jackInTheBox");
+          }, 7e3);
+      }
+    },
+    questionsArray = [],
+    questionOnHousePreference = t.filter(function (e) {
+      return "Which house do you hope to be in?" === e.question;
+    })[0],
+    questionsNotOnHousePreference = shuffleArray(
+      t.filter(function (e) {
+        return questionOnHousePreference !== e;
+      })
+    ),
+    getQuestionArray = function () {
+      for (var e = 0; e < quizLength - 1; e++)
+        questionsArray.push(questionsNotOnHousePreference[e]);
+      questionsArray.push(questionOnHousePreference);
+    },
+    lengthSelectorBtns = document.querySelectorAll(".length-selector-btn"),
+    _loop_1 = function (e) {
+      e.addEventListener("click", function () {
+        handleLengthSelection(e.id), getQuestionArray(), proceed();
+      });
+    },
+    _i = 0,
+    lengthSelectorBtns_1 = lengthSelectorBtns;
+  _i < lengthSelectorBtns_1.length;
+  _i++
+)
+  _loop_1(lengthSelectorBtns_1[_i]);
